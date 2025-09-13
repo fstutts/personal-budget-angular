ddbab639b9a4c82bcbd9bad15190e62d59457ca6
Forrest Stutts <fstutts@Mac.lan>
2025-09-13 12:19:38 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,25 +1,12 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget:[
-    {
-        title: 'Eat out',
-        budget: 25
-    },
-    {
-        title: 'Rent',
-        budget: 375
-    },
-    {
-        title: 'Grocery',
-        budget: 110
-    },
-]
-};
+// Read budget data from JSON file
+const budget = JSON.parse(fs.readFileSync('budget.json', 'utf8'));
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
