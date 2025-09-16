5d4f5103cae71657a06e36812309bc55175d70f6
Forrest Stutts <fstutts@Mac.lan>
2025-09-15 21:02:48 -0400

    Added CORS

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+//Budget API
+
 const express = require('express');
+const cors = require('cors');
 const fs = require('fs');
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.use('/', express.static('public'));
 
@@ -17,5 +22,5 @@ app.get('/budget', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`API served at http://localhost:${port}`)
 });
